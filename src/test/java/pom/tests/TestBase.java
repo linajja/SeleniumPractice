11813bc17cf7991539fb2c8be4e554a3ce07168c
linajja <lina.github@gmail.com>
2023-05-10 21:19:25 +0300

    Add TestBase class

--- src/test/java/pom/tests/TestBase.java
+++ src/test/java/pom/tests/TestBase.java
@@ -5,11 +5,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import pom.pages.Common;
 
-
 @Listeners()
 public abstract class TestBase {
     @BeforeMethod
     public abstract void setUp();
+
     @AfterMethod
     public void tearDown() {
         Common.closeDriver();
